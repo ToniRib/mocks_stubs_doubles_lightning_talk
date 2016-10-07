25fd75fda690aa359fbbd414352ae4bca461ea26
ToniRib <tonimarierib@gmail.com>
2016-10-06 20:33:06 -0600

    Add preference methods to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,20 @@ class User < ActiveRecord::Base
   belongs_to :company
 
   has_many :addresses
+
+  def prefers_email?
+    notification_preference == 'email'
+  end
+
+  def prefers_snail_mail?
+    notification_preference == 'mail'
+  end
+
+  def prefers_text?
+    notification_preference == 'text'
+  end
+
+  def prefers_phone_call?
+    notification_preference == 'phone'
+  end
 end
