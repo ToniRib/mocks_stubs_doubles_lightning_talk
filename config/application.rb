d81edde605a4767052ff03eab9c6953c84e07b1a
ToniRib <tonimarierib@gmail.com>
2016-10-10 09:57:57 -0600

    Autoload all lib files

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ShipIt
@@ -28,6 +26,7 @@ module ShipIt
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.autoload_paths << Rails.root.join('lib/services')
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
