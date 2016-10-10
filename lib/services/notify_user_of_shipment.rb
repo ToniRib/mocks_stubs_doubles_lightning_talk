4c5596aaa68e5873d2af1311eec0df83c672ce61
ToniRib <tonimarierib@gmail.com>
2016-10-10 11:04:56 -0600

    Refactor to make the service more clear

--- lib/services/notify_user_of_shipment.rb
+++ lib/services/notify_user_of_shipment.rb
@@ -4,13 +4,15 @@ class NotifyUserOfShipment
   end
 
   def send_notification
-    if user.prefers_email?
-      NotifyViaEmail.new(order).call
-    elsif user.prefers_snail_mail?
-      NotifyViaSnailMail.new(order).call
-    elsif user.prefers_text?
-      NotifyViaText.new(order).call
-    end
+    service = if user.prefers_email?
+                NotifyViaEmail.new(order)
+              elsif user.prefers_snail_mail?
+                NotifyViaSnailMail.new(order)
+              elsif user.prefers_text?
+                NotifyViaText.new(order)
+              end
+
+    service.call
   end
 
   private
